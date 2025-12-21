9b208d82f5fac948a869245ccd18c1277a908c2f
saurabhpatil67 <saurab.nik2002@gmail.com>
2025-12-21 12:47:41 +0530

    feat: complete Hero section and Feature section with responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import './App.css'
+import FeatureSection from './components/FeatureSection'
+import HeroSection from './components/HeroSection'
 import Navbar from './components/Navbar'
 
 function App() {
@@ -7,6 +9,10 @@ function App() {
   return (
     <>
       <Navbar/>
+      <div className='max-w-7xl mx-auto pt-20 px-6'>
+      <HeroSection/>
+      <FeatureSection/>
+      </div>
     </>
   )
 }
