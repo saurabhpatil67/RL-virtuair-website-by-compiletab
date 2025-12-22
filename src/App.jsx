8e5406bec94a1a89bc2ed4d43fba380f49a181c8
saurabhpatil67 <saurab.nik2002@gmail.com>
2025-12-22 22:15:53 +0530

    feat: Pricing, Workflow, Testimonial and footer section completed with responsive code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import './App.css'
 import FeatureSection from './components/FeatureSection'
+import Footer from './components/Footer'
 import HeroSection from './components/HeroSection'
 import Navbar from './components/Navbar'
+import Pricing from './components/Pricing'
+import Testimonials from './components/Testimonials'
+import Workflow from './components/Workflow'
 
 function App() {
   
@@ -12,6 +16,10 @@ function App() {
       <div className='max-w-7xl mx-auto pt-20 px-6'>
       <HeroSection/>
       <FeatureSection/>
+      <Workflow/>
+      <Pricing/>
+      <Testimonials/>
+      <Footer/>
       </div>
     </>
   )
